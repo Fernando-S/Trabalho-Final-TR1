8c1dd41707388cbdd4869d3a940756591a7efe4f
Fernando <nandoo44@hotmail.com>
2019-11-18 14:32:06 -0300

    Arrumei a CamadaEnlace.h que estava vazia

---
+++ CamadaEnlace.h
@@ -0,0 +1,86 @@
+#ifndef CAMADA_ENLACE_H
+#define CAMADA_ENLACE_H
+
+void CamadaEnlaceDadosTransmissora (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramento (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErro (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeFluxo (int quadro []);
+
+void CamadaEnlaceDadosTransmissora (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramento (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramentoInsercaoDeBytes (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramentoInsercaoDeBits (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramentoViolacaoDaCamadaFisica (int quadro []);
+
+void CamadaEnlaceDadosReceptora (int quadro []);
+
+void CamadaEnlaceDadosReceptoraEnquadramento (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErro (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeFluxo (int quadro []);
+
+void CamadaEnlaceDadosReceptora (int quadro []);
+
+void CamadaEnlaceDadosReceptoraEnquadramento (int quadro []);
+
+void CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres (int quadro []);
+
+void CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes (int quadro []);
+
+void CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBits (int quadro[]);
+
+void CamadaEnlaceDadosReceptoraEnquadramentoViolacaoDaCamadaFisica(int quadro []);
+
+void CamadaEnlaceDadosTransmissora (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraEnquadramento (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErro (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeFluxo (int quadro []);
+
+void CamadaEnlaceDadosTransmissora (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErro (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErroBitParidadePar (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErroBitParidadeImpar (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErroCRC (int quadro []);
+
+void CamadaEnlaceDadosTransmissoraControleDeErroCodigoDeHamming (int quadro []);
+
+void MeioDeComunicacao (int fluxoBrutoDeBits []);
+
+void CamadaEnlaceDadosReceptora ( int quadro []);
+
+void CamadaEnlaceDadosReceptoraEnquadramento ( int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErro ( int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeFluxo ( int quadro []);
+
+void CamadaEnlaceDadosReceptora (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErro (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadePar (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadeImpar (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErroCRC (int quadro []);
+
+void CamadaEnlaceDadosReceptoraControleDeErroCodigoDeHamming (int quadro []);
+
+#endif  // CAMADA_ENLACE_H
