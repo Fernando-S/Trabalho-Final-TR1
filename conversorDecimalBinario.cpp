e5eaa69735d5e1cf51fb07524ffbb0a8f80ab057
Aécio <30986743+afgmlff@users.noreply.github.com>
2019-11-18 19:56:38 -0300

    Add files via upload

--- conversorDecimalBinario.cpp
+++ conversorDecimalBinario.cpp
@@ -13,31 +13,33 @@ int n = msg.size();
 int mensagem[n];
 int i = 0, m = 0;
 int quadro_invertido[n*7], quadro[n*7];
-
+int size = 0;
 for (i = 0; i < n; i++){
   mensagem[i] = msg[i];
 }
-/*  Binario
+
+i=0;
+int o = 0, soma = 0;
 for(m = 0; m < n; m++){
+  i = 0;
   while(mensagem[m] > 0){
-    quadro_invertido[i] = mensagem[m] % 2;
-    mensagem[m] = mensagem[m]/2;
+    quadro_invertido[i] = mensagem[m]%2;
+    mensagem[m] /= 2;
     i++;
+  }
+  size += i;
+  int j = i-1;
+  soma = o + i;
+  while(o < (soma)){
+    quadro[o] = quadro_invertido[j];
+    o++;
+    j--;
   }
 }
 
-int j = n*7;
 
-for(i = 0; i < n*7; i++){
-  quadro[i] = quadro_invertido[j];
-  j--;
-}
-
-*/
-for (i = 0; i < n; i++){
-//  quadro[i] = msg[i];
-  cout << mensagem[i];
-//  cout << " ";
+for (m = 0; m < size; m++){
+  cout << quadro[m];
 }
 
   return 0;
