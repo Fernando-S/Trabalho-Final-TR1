0ffe52e4a6bbb5639ffac6f0c10356878bb76c73
Fernando <nandoo44@hotmail.com>
2019-11-19 23:26:06 -0300

    Possibilidade de ecolha da codificação pelo usuário

--- CamadaFisica.h
+++ CamadaFisica.h
@@ -17,18 +17,18 @@ void CamadaFisicaTransmissoraCodificacaoBinaria (int quadro [], int size);
  *    quadro, vetor de valores binários.
  *    quadro_size, quantidade de elementos do quadro.
  *    manchester, deve ser um vetor de tamanho quadro_size*2 (já alocado). O
- *        quadro com a codificação Manchester estará nesses endereços após a 
+ *        quadro com a codificação Manchester estará nesses endereços após a
  *        execução desta função.
  */
 void CamadaFisicaTransmissoraCodificacaoManchester (int quadro[], int quadro_size, int* manchester);
 
 void CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(int quadro [], int size);
 
-void MeioDeComunicacao (int fluxoBrutoDeBits [], int size);
+void MeioDeComunicacao (int fluxoBrutoDeBits [], int size, int tipoDeDecodificacao);
 
-void CamadaFisicaReceptora (int quadro[], int size);
+void CamadaFisicaReceptora (int quadro[], int size, int tipoDeDecodificacao);
 
-int* CamadaFisicaReceptoraCodificacaoBinaria (int quadro [], int size);
+void CamadaFisicaReceptoraCodificacaoBinaria (int quadro [], int size);
 
 void CamadaFisicaReceptoraDecodificacaoManchester (int* manchester, int size, int* quadro);
 
