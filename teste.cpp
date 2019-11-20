d95adf4ca9808482c7c69cb89886764d3cbe09e2
Guilherme Fleury <guilherme.fleury26@gmail.com>
2019-11-20 02:17:44 -0300

    função binarioparastring correta funcionando

--- teste.cpp
+++ teste.cpp
@@ -1,49 +1,45 @@
+#include "CamadaFisica.h"
+#include <bits/stdc++.h>
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <bits/stdc++.h>
-#include "CamadaFisica.h"
 
 using namespace std;
 
-string binarioparastring(int* vetor, int n) {
-    int k, c, i = 0, j = 0;
-    char retorno[n / 8];
+string binarioparastring(int *vetor, int n) {
+  int k, c, j = 0;
+  uint i = 0;
 
-    for (c = 0; c < n; c += 8) {
-        j = 0;
-        for (k = 0; k < 8; k++) {
-            i = (vetor[n] * 128) + (vetor[n + 1] * 64) + (vetor[n + 2] * 32) +
-                (vetor[n + 3] * 16) + (vetor[n + 4] * 8) + (vetor[n + 5] * 4) + (vetor[n + 6] * 2) + (vetor[n + 7] * 1);
-            retorno[j] = i;
-            j++;
-        }
-    }
+  std::vector<char> ret;
+  for (c = 0; c < n; c += 8) {
+    j = 0;
 
-    return retorno;
+    i = (vetor[c] * 128) + (vetor[c + 1] * 64) + (vetor[c + 2] * 32) +
+        (vetor[c + 3] * 16) + (vetor[c + 4] * 8) + (vetor[c + 5] * 4) +
+        (vetor[c + 6] * 2) + (vetor[c + 7] * 1);
+    std::cout << i << std::endl;
+    std::cout << static_cast<char>(i) << std::endl;
+    ret.push_back(static_cast<char>(i));
+    j++;
+  }
+
+  return std::string(ret.data());
 }
 
 ///////////////////////////
 //		Metodo Main		//
 /////////////////////////
-int main (void) {
-    int a[8] = {0,0,0,0,0,0,0,1};
-    char b = 94;
-    char c[3] = {97, 97, 97};
-    string d = binarioparastring(a, 8);
-    char* e;
+int main(void) {
+  int a[8] = {0, 1, 0, 1, 1, 1, 0, 1};
+  char f[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+  char c[3] = {97, 97, 97};
+  string d = binarioparastring(a, 8);
+  char *e;
 
-    for(int i = 0; i < 8; i++){
-        cout << d[i] << endl;
-//        e[i] = d[i];  isso aqui da segmentation fault
-    }
+  cout << "d eh: " << d << endl;
+  //   cout << "b eh: " << b << endl;
+  cout << "c eh: " << c << endl;
+  cout << "e eh: " << e << endl;
 
-    cout << "d eh: " << d << endl;
-    cout << "b eh: "  << b << endl;
-    cout << "c eh: "  << c << endl;
-    cout << "e eh: "  << e << endl;
-
-    return 0;
+  return 0;
 }
-
-
