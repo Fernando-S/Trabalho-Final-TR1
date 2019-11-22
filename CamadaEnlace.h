5ac3cd765cd06dd820ef78aeef5b3a4cd65b8b72
nmschumacher <nicolasmschumacher@gmail.com>
2019-11-22 02:51:35 -0200

    Ajuste dos comentários do .h de enlace

--- CamadaEnlace.h
+++ CamadaEnlace.h
@@ -33,13 +33,9 @@ void CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBytes (int quadro [], int 
 void CamadaDeEnlaceTransmissoraEnquadramentoInsercaoDeBits (int quadro [], int size);
 
 /*
+
 void CamadaEnlaceDadosTransmissoraEnquadramentoViolacaoDaCamadaFisica (int quadro[]);
 
-void CamadaEnlaceDadosReceptora (int quadro[]);
-
-void CamadaEnlaceDadosReceptoraEnquadramento (int quadro[]);
-
-void CamadaEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres (int quadro[], size);
 */
 
 void CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes (int quadro [], int size);
@@ -48,9 +44,9 @@ void CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes (int quadro [], int 
 void CamadaEnlaceDadosReceptoraEnquadramentoInsercaoDeBits (int quadro[], int size);
 
 /*
+
 void CamadaEnlaceDadosReceptoraEnquadramentoViolacaoDaCamadaFisica(int quadro[]);
 
-void CamadaEnlaceDadosTransmissoraControleDeFluxo (int quadro[]);
 */
 
 void CamadaEnlaceDadosTransmissoraControleDeErro (int quadro[], int size);
@@ -75,35 +71,11 @@ void CamadaEnlaceDadosTransmissoraControleDeErroBitParidadePar (int quadro[], in
  *          paridade seja ímpar.
 */
 void CamadaEnlaceDadosTransmissoraControleDeErroBitParidadeImpar (int quadro[], int size_quadro);
-/*
 
+/*
 void CamadaEnlaceDadosTransmissoraControleDeErroCRC (int quadro[]);
 
 void CamadaEnlaceDadosTransmissoraControleDeErroCodigoDeHamming (int quadro[]);
-
-void CamadaEnlaceDadosReceptoraControleDeFluxo ( int quadro[]);
-
-void CamadaEnlaceDadosReceptoraControleDeErro (int quadro[]);
-
-/* Calcula o bit de paridade do quadro recebido, retorna 1 no caso em que a
- * transmissão foi bem sucedida, 0 caso contrário.
- * Args:
- *    quadro, vetor de bits.
- *    size_quadro, número de elementos do quadro.
- * Return:
- *    sucesso, valor 1 ou 0 indicando o sucesso da operação ou não, respectivamente.
-
-void CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadePar (int quadro[], int size);
-
-/* Calcula o bit de paridade do quadro recebido, retorna 1 no caso em que a
- * transmissão foi bem sucedida, 0 caso contrário.
- * Args:
- *    quadro, vetor de bits.
- *    size_quadro, número de elementos do quadro.
- * Return:
- *    sucesso, valor 1 ou 0 indicando o sucesso da operação ou não, respectivamente.
-
-void CamadaEnlaceDadosReceptoraControleDeErroBitDeParidadeImpar (int quadro[], int size);
 
 /*
 void CamadaEnlaceDadosReceptoraControleDeErroCRC (int quadro[]);
